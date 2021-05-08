b41edfde226eda6104c003f0555e14d0b28edcbf
Peter Torres <torres7p@gmail.com>
2021-05-08 01:07:50 -0700

    Added matches drawer check test and fixed tag in settings fragment

--- app/src/androidTest/java/com/example/helloworld/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/SecondActivityTest.java
@@ -1,5 +1,7 @@
 package com.example.helloworld;
 
+import android.view.Gravity;
+
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -9,6 +11,7 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -20,8 +23,18 @@ public class SecondActivityTest {
 
     @Test
     public void profileDrawerCheck() {
-        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_matches));
         onView(allOf(withId(R.id.matchesTextView))).check((matches(withText("Your matches go here!"))));
     }
+
+    @Test
+    public void matchesDrawerCheck() {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_settings));
+        onView(allOf(withId(R.id.settingsTextView))).check((matches(withText("Settings go here!"))));
+    }
+
 }
