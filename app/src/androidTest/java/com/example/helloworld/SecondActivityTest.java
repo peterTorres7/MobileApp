11011f2e413d7184a45bc05a3f780141e0ba1115
Peter Torres <torres7p@gmail.com>
2021-05-08 00:05:56 -0700

    Added second activity profile drawer check

--- app/src/androidTest/java/com/example/helloworld/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/SecondActivityTest.java
@@ -1,11 +1,27 @@
 package com.example.helloworld;
 
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class SecondActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void profileDrawerCheck() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_matches));
+        onView(allOf(withId(R.id.matchesTextView))).check((matches(withText("Your matches go here!"))));
+    }
 }
