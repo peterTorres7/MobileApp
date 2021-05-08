f4ecbbbeb0a5f842f60302810c49ed7cb2755c0e
Peter Torres <torres7p@gmail.com>
2021-05-07 19:58:01 -0700

    Added a test

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -29,11 +31,21 @@ public class MainActivityTest {
                 .check(matches(withText("Peter Torres 4/26/21")));
     }
 
-//    @Test
-//    public void userCanSignUp() throws InterruptedException {
-//        onView(withId(R.id.nameEditText)).perform(typeText("Diego"));
-//
-//    }
+    @Test
+    public void userCanSignUp() throws InterruptedException {
+        onView(withId(R.id.nameEditText)).perform(typeText("Diego"));
+        onView(withId(R.id.ageEditText)).perform(typeText("12"));
+        onView(withId(R.id.occupationEditText)).perform(typeText("Dog"));
+        onView(withId(R.id.descriptionEditText)).perform(typeText("Chill"));
+
+        onView(withId(R.id.submitButton)).perform(scrollTo(),(click()));
+
+        onView(allOf(withId(R.id.nameTextView))).check(matches(withText("Diego")));
+        onView(allOf(withId(R.id.ageTextView))).check(matches(withText("12")));
+        onView(allOf(withId(R.id.occupationTextView))).check(matches(withText("Dog")));
+        onView(allOf(withId(R.id.descriptionTextView))).check(matches(withText("Chill")));
+
+    }
 
 //    @Test
 //    public void checkName() {
