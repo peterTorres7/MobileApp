601b63ec06d9bdf37b85f92fb47f186b82de80dd
Peter Torres <torres7p@gmail.com>
2021-05-11 23:13:27 -0700

    Added like button with toast.

--- app/src/main/java/com/example/helloworld/MatchesCardViewHolder.java
+++ app/src/main/java/com/example/helloworld/MatchesCardViewHolder.java
@@ -1,8 +1,10 @@
 package com.example.helloworld;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,10 +13,20 @@ public class MatchesCardViewHolder extends RecyclerView.ViewHolder {
 
     ImageView matchesImage;
     TextView matchesName;
+    Button likeButton;
 
     public MatchesCardViewHolder(@NonNull View itemView) {
         super(itemView);
         matchesImage = itemView.findViewById(R.id.matchesImageView);
         matchesName = itemView.findViewById(R.id.matchesNameTextView);
+        likeButton = itemView.findViewById(R.id.likeButton);
+
+        likeButton.setOnClickListener(view -> {
+            String likeMsg = "You liked " + matchesName.getText().toString() + "!";
+            Toast toast = Toast.makeText(view.getContext(), likeMsg, Toast.LENGTH_LONG);
+            View toastView = toast.getView();
+            toastView.setBackgroundResource(R.drawable.like_icon);
+            toast.show();
+        });
     }
 }
