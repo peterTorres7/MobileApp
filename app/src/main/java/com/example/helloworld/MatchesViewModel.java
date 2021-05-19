a7f336a1f51b01500ca0e129a6f786f3b77af402
Peter Torres <torres7p@gmail.com>
2021-05-18 23:41:49 -0700

    Added matches view model

--- app/src/main/java/com/example/helloworld/MatchesViewModel.java
+++ app/src/main/java/com/example/helloworld/MatchesViewModel.java
@@ -1,16 +1,37 @@
-//package com.example.helloworld;
-//
-//import java.util.ArrayList;
-//import java.util.function.Consumer;
-//
-//public class MatchesViewModel {
-//    private MatchesDataModel matchesDataModel;
-//
-//    public MatchesViewModel() {
-//        this.matchesDataModel = new MatchesDataModel();
-//    }
-//
-//    public void getMatches(Consumer<ArrayList<MatchesModel>> responseCallback) {
-//        matchesDataModel.getMatches()
-//    }
-//}
+package com.example.helloworld;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class MatchesViewModel {
+    private MatchesDataModel matchesDataModel;
+
+    public MatchesViewModel() {
+        matchesDataModel = new MatchesDataModel();
+    }
+
+    public void getMatches(Consumer<ArrayList<MatchesModel>> resultCallback) {
+        matchesDataModel.getMatches(
+                (QuerySnapshot querySnapShot) -> {
+                    if (querySnapShot != null) {
+                        ArrayList<MatchesModel> matches = new ArrayList<>();
+                        for (DocumentSnapshot matchSnapshot : querySnapShot.getDocuments()) {
+                            MatchesModel matchesModel = matchSnapshot.toObject(MatchesModel.class);
+                            assert matchesModel != null;
+                            matchesModel.uid = matchSnapshot.getId();
+                            matches.add(matchesModel);
+                        }
+                        resultCallback.accept(matches);
+                    }
+                },
+                (databaseError -> System.out.println("Looks like something went wrong!" + databaseError))
+        );
+    }
+
+    public void clear() {
+        matchesDataModel.clear();
+    }
+}
