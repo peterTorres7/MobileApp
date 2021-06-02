34dafedf8080f8fc6b545b0b0e055efae93b34ce
Peter Torres <torres7p@gmail.com>
2021-06-01 23:35:47 -0700

    Start of location services

--- app/src/main/java/com/example/helloworld/MatchesFragment.java
+++ app/src/main/java/com/example/helloworld/MatchesFragment.java
@@ -1,11 +1,22 @@
 package com.example.helloworld;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +28,7 @@ import java.util.List;
 public class MatchesFragment extends Fragment {
 
     MatchesData matchesData;
+    LocationManager locationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,7 @@ public class MatchesFragment extends Fragment {
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_matches, container, false);
+        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -58,5 +71,59 @@ public class MatchesFragment extends Fragment {
 //        recyclerView.setAdapter(adapter);
         return view;
     }
+
+    public boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this.getContext());
+        dialog.setTitle(R.string.enable_location)
+                .setMessage(getString(R.string.location_message))
+                .setPositiveButton(R.string.location_settings, (paramDialogInterface, paramInt) -> {
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                })
+                .setNegativeButton(R.string.location_cancel, (paramDialogInterface, paramInt) -> {});
+        dialog.show();
+    }
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+//            longitudeNetwork = location.getLongitude();
+//            latitudeNetwork = location.getLatitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
+
+//    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED) ||
+//            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED) {
+//        Location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60 * 1000, 10,
+//                locationListenerNetwork);
+//        Toast.makeText(this, R.string.network_provider_started_running, Toast.LENGTH_LONG).show());
+//        button.setText(R.string.pause);
+//    }
+//
+//    locationManager.removeUpdates(locationListenerNetwork)
+//    button.setText(R.string.resume);
 
 }
